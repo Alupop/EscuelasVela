14812a947b9c0fcb363b1bafc8513f046470bf51
George Ionut Popa <alupop7602@ieselcaminas.org>
2024-01-15 19:19:17 +0100

    Haciendo Views

--- addons/escuelas_vela/models/models.py
+++ addons/escuelas_vela/models/models.py
@@ -1,18 +1,48 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields
 
+class Escuela(models.Model):
+    _name = 'escuelas_vela.escuela'
+    _description = 'Escuela de Vela'
 
-# class escuelas_vela(models.Model):
-#     _name = 'escuelas_vela.escuelas_vela'
-#     _description = 'escuelas_vela.escuelas_vela'
+    name = fields.Char(string='Denominación', required=True)
+    logotipo = fields.Binary(string='Logotipo')
+    direccion = fields.Char(string='Dirección')
+    telefono = fields.Char(string='Teléfono')
+    email = fields.Char(string='Correo Electrónico')
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+class Curso(models.Model):
+    _name = 'escuelas_vela.curso'
+    _description = 'Curso de Vela'
+
+    title = fields.Char(string='Título', required=True)
+    duracion_dias = fields.Integer(string='Duración en días')
+    duracion_horas = fields.Integer(string='Duración en horas')
+    precio = fields.Float(string='Precio')
+
+    escuela_id = fields.Many2one('escuelas_vela.escuela', string='Escuela')
+
+class Monitor(models.Model):
+    _name = 'escuelas_vela.monitor'
+    _description = 'Monitor de Vela'
+
+    name = fields.Char(string='Nombre', required=True)
+    telefono = fields.Char(string='Teléfono')
+    email = fields.Char(string='Correo Electrónico')
+    direccion = fields.Char(string='Dirección')
+    codigo_identificacion = fields.Char(string='Código de identificación', required=True, unique=True)
+
+    escuela_ids = fields.Many2many('escuelas_vela.escuela', string='Escuelas colaboradoras')
+
+class Alumno(models.Model):
+    _name = 'escuelas_vela.alumno'
+    _description = 'Alumno de Vela'
+
+    name = fields.Char(string='Nombre', required=True)
+    telefono = fields.Char(string='Teléfono')
+    email = fields.Char(string='Correo Electrónico')
+    direccion = fields.Char(string='Dirección')
+    numero_matricula = fields.Char(string='Número de matrícula', required=True)
+    
+    escuela_id = fields.Many2one('escuelas_vela.escuela', string='Escuela')
